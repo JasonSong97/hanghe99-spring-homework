71edc8e94c899482db0a35fdc3d536fc4036e458
JasonSong97 <jaegeunsong97@gmail.com>
2024-12-12 22:29:25 +0900

    Initial commit

---
+++ src/main/java/com/example/hangge/HanggeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanggeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanggeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hangge/HanggeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanggeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
